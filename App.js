4ef8e65edc3663b5bea59be4e332a1ca701547a2
Islam <aissiislam97@gmail.com>
2021-12-04 22:49:27 +0100

    [MAJOR] Display FieldSceen component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FieldScreen from './screens/FieldScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FieldScreen />
       <StatusBar style="auto" />
     </View>
   );
